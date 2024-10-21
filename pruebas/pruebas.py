6a4d8033a150a8ac9299091c001dc94634001c39
pokkzcl <luis.contcast@icloud.com>
2024-10-21 07:59:28 -0300

    test

--- pruebas/pruebas.py
+++
@@ -1,9 +0,0 @@
-import os
-import datetime
-
-os.system('cls' if os.name == 'nt' else 'clear')
-
-fecha_inicio_contrato = input("Fecha Inicio Contrato DD-MM-YYYY: ")
-fecha_contrato = datetime.datetime.strptime(fecha_inicio_contrato, '%d-%m-%Y')
-
-print(fecha_contrato)
